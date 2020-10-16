bd0f5de2c011929c19a4b1153930c7b24c1036d5
MarijaDordevic <marija.djordjevic@izettle.com>
2020-10-16 16:06:01 +0200

    Finished game screen

--- src/Views/FinishedGame.elm
+++ src/Views/FinishedGame.elm
@@ -1,6 +1,8 @@
 module Views.FinishedGame exposing (..)
 
+import Array
 import Game.Game exposing (Game)
+import Game.Teams exposing (getScoreboard)
 import Game.Words exposing (Word)
 import Html exposing (Html, div, h2, h3, h4, span, text)
 import Html.Attributes exposing (class)
@@ -8,28 +10,31 @@ import State exposing (Msg)
 import Views.Playing.Teams exposing (teamsView)
 
 
-wordView : Word -> Html Msg
-wordView word =
-    div []
-        [ h2 []
-            [ text word.word ]
-        , h4
-            []
-            [ text word.player ]
-        ]
-
-
 finishedGameView : Game -> Html Msg
 finishedGameView game =
-    div []
-        [ h2 []
-            [ text "The game is finished" ]
+    let
+        winningTeam =
+            getScoreboard game.state.teams
+                |> List.head
+                |> Maybe.map
+                    (\team ->
+                        team.players
+                            |> List.map .name
+                            |> String.join " & "
+                            |> text
+                    )
+                |> Maybe.withDefault (text "")
+
+        confetti =
+            List.range 0 200
+                |> List.map (\index -> div [ class ("confetti-" ++ String.fromInt index) ] [])
+                |> div [ class "confetti" ]
+    in
+    div [ class "finished-game-container" ]
+        [ confetti
+        , h2 []
+            [ text "And the winners are" ]
+        , span [ class "winners" ] [ winningTeam ]
+        , span [ class "medal" ] [ text "\u{1F947}" ]
         , teamsView game
-        , div
-            [ class "words-overview-wrapper " ]
-            [ h3 [] [ text "Words overview" ]
-            , game.state.words.next
-                |> List.map wordView
-                |> div [ class "words-overview-container" ]
-            ]
         ]
